dd5557f1c8e374abe375f61de25aacef3b5c12b0
Nick.Harvey <Nick.Harvey@veteransunited.com>
2023-10-14 11:05:45 -0500

    neotree config

--- init.lua
+++ init.lua
@@ -231,6 +231,71 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      {
+        's1n7ax/nvim-window-picker',
+        version = '2.*',
+        config = function()
+            require 'window-picker'.setup({
+                filter_rules = {
+                    include_current_win = false,
+                    autoselect_one = true,
+                    -- filter using buffer options
+                    bo = {
+                        -- if the file type is one of following, the window will be ignored
+                        filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                        -- if the buffer type is one of following, the window will be ignored
+                        buftype = { 'terminal', "quickfix" },
+                    },
+            },
+        })
+        end,
+      }
+    },
+    config = function()
+      vim.keymap.set('n', '<C-\\>', ':Neotree last reveal<CR>', { noremap = true, silent = true })
+
+      require("neo-tree").setup({
+        source_selector = {
+          winbar = true,
+        },
+        close_if_last_window = true,
+        enable_git_status = true,
+        enable_normal_mode_for_inputs = true,
+        open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+        sort_case_insensitive = true,
+        default_component_configs = {
+          container = {
+            enable_character_fade = true
+          },
+          name = {
+            trailing_slash = false,
+            use_git_status_colors = true,
+            highlight = "NeoTreeFileName",
+          },
+        },
+        filesystem = {
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          },
+        },
+        buffers = {
+          follow_current_file = {
+            enabled = true, -- This will find and focus the file in the active buffer every time
+            --              -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          },
+        },
+      })
+    end,
+  },
 }, {})
 
 -- [[ Setting options ]]
