1b25c11c1ee80b04f9f344e87970b63dfea3ec0b
Nick Harvey <Nick.Harvey.Developer@gmail.com>
2023-10-19 19:52:27 -0500

    line numbers in the tree window for fast nav

--- init.lua
+++ init.lua
@@ -262,6 +262,23 @@ require('lazy').setup({
       vim.keymap.set('n', '<C-\\>', ':Neotree last reveal<CR>', { noremap = true, silent = true })
 
       require("neo-tree").setup({
+        event_handlers = {
+          -- {
+          --   event = "file_opened",
+          --   handler = function(file_path)
+          --     -- auto close
+          --     -- vimc.cmd("Neotree close")
+          --     -- OR
+          --     require("neo-tree.command").execute({ action = "close" })
+          --   end
+          -- },
+          {
+            event = "neo_tree_window_after_open",
+            handler = function(args)
+              vim.cmd("set number")
+            end
+          },
+        },
         source_selector = {
           winbar = true,
         },
